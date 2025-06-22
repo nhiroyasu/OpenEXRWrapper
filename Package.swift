f322c1b4943cea7885f3a20ec97c367caf8aa5ff
nhiroyasu <nhiro1109pri@gmail.com>
2025-06-22 17:18:53 +0900

    Update binaryTarget url

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: NAME,
-            url: "https://github.com/nhiroyasu/OpenEXRWrapper/releases/download/untagged-874d449a6c400b74e6b6/OpenEXRWrapper.xcframework.zip",
+            url: "https://github.com/nhiroyasu/OpenEXRWrapper/releases/download/try-publishing-xcframework/OpenEXRWrapper.xcframework.zip",
             checksum: "039509b39da702faab935252aa8deccd46b0138146b1d9bcb35f9acad4201a81"
         )
     ]
